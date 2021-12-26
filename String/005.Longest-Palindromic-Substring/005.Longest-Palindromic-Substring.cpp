da536d4a12621d070a87fb3167eb82aa7886703b
Pulkit Bajaj <50499842+pulkit171112236@users.noreply.github.com>
2021-12-27 00:07:09 +0530

    Update 005.Longest-Palindromic-Substring.cpp

--- String/005.Longest-Palindromic-Substring/005.Longest-Palindromic-Substring.cpp
+++ String/005.Longest-Palindromic-Substring/005.Longest-Palindromic-Substring.cpp
@@ -1,50 +1,48 @@
-
+// MANACHER'S ALGORITHM
 class Solution {
-public:
-    string longestPalindrome(string s) 
-    {
-        string t="#";
-        for (int i=0; i<s.size(); i++)
-            t+= s.substr(i,1)+"#";
-        
-        int N = t.size();
-        vector<int>P(N,0);
-        int maxCenter = -1;
-        int maxRight = -1;
-        
-        for (int i=0; i<N; i++)
-        {
-            int k;
-            if (i > maxRight)
-            {
-                k = 0;
-                while (i-k>=0 && i+k<N && t[i-k]==t[i+k]) k++;
+   public:
+    string longestPalindrome(string s) {
+        // max-length of palindromic string with center
+        vector<int> memo;
+        int len, L, R, C, mirr, p, q, max_center, max_len, org_pos;
+        string ss;
+
+        ss = "#";
+        for (int i = 0; i < s.length(); i++) {
+            ss.push_back(s[i]);
+            ss.push_back('#');
+        }
+        len = ss.length();
+        memo.resize(len);
+        L = R = C = 0;
+        for (int i = 0; i < len; i++) {
+            mirr = 2 * C - i;
+            // if i lies in current pal
+            if (i <= R) {
+                memo[i] = min(memo[mirr], R - i);
             }
-            else
-            {
-                k = min(P[maxCenter*2-i],maxRight-i);
-                while (i-k>=0 && i+k<N && t[i-k]==t[i+k]) k++;                
+            p = i - memo[i];
+            q = i + memo[i];
+            while (p - 1 >= 0 && q + 1 < len && ss[p - 1] == ss[q + 1]) {
+                p--;
+                q++;
+                memo[i]++;
             }
-            P[i] = k-1;
-            if (i + P[i] > maxRight)
-            {
-                maxRight = i + P[i];
-                maxCenter = i;
+            if (q > R) {
+                L = p;
+                R = q;
+                C = i;
             }
         }
-        
-        // for (auto x:P) cout<<x<<" ";
-        
-        int maxLen = -1;
-        int center;
-        for (int i=0; i<P.size(); i++)
-        {
-            if (P[i]>maxLen)
-            {
-                maxLen = P[i];
-                center = i;                
+        max_center = -1;
+        max_len = -1;
+        for (int i = 0; i < memo.size(); i++) {
+            if (memo[i] > max_len) {
+                max_len = memo[i];
+                max_center = i;
             }
         }
-        return s.substr(center/2 - maxLen/2, maxLen);
+        org_pos = (max_center - max_len) / 2;
+        return s.substr(org_pos, max_len);
     }
 };
